083e0cb027f23c900492acb79f0a72bab60476c8
vinh <nxv109@gmail.com>
2019-04-17 08:29:13 +0700

    Updates

---
+++ precache-manifest.f92e6d80a9ee5f3d3ba00df25bd2e55a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "0422b42cc2b98d7ae07c0022db731193",
+    "url": "/nguyen-tuan-art/static/media/pupg-1.0422b42c.jpg"
+  },
+  {
+    "revision": "1b4ba8a11f0dd9fda616",
+    "url": "/nguyen-tuan-art/static/css/main.62f334c2.chunk.css"
+  },
+  {
+    "revision": "37e6d8fb5020c16b96e5",
+    "url": "/nguyen-tuan-art/static/js/runtime~main.7c7270b4.js"
+  },
+  {
+    "revision": "ab8f247aa13204f7478a",
+    "url": "/nguyen-tuan-art/static/js/2.407b3739.chunk.js"
+  },
+  {
+    "revision": "f7c56d6600e898d868221ce9b9199362",
+    "url": "/nguyen-tuan-art/static/media/logo-desktop.f7c56d66.png"
+  },
+  {
+    "revision": "ef323512f440d0cc495def8089334318",
+    "url": "/nguyen-tuan-art/static/media/ed--4.ef323512.jpg"
+  },
+  {
+    "revision": "cf066f46734de9ad4f5de77da9265510",
+    "url": "/nguyen-tuan-art/static/media/8.cf066f46.jpg"
+  },
+  {
+    "revision": "8889e54f2fd96cf19d503ab93b0ad7bf",
+    "url": "/nguyen-tuan-art/static/media/*-3.8889e54f.jpg"
+  },
+  {
+    "revision": "558de3268c69fe7f5a8b685782ed1fc5",
+    "url": "/nguyen-tuan-art/static/media/about-me.558de326.jpg"
+  },
+  {
+    "revision": "f829f9d67f406e294004d14c6fec7cdb",
+    "url": "/nguyen-tuan-art/static/media/background-1.f829f9d6.JPG"
+  },
+  {
+    "revision": "b8f09e19bccdb646cfa9d039589b79db",
+    "url": "/nguyen-tuan-art/static/media/pupg-3.b8f09e19.jpg"
+  },
+  {
+    "revision": "1b4ba8a11f0dd9fda616",
+    "url": "/nguyen-tuan-art/static/js/main.d643b3b4.chunk.js"
+  },
+  {
+    "revision": "1521f8d1fb8ddf2ab3ee578c053bf268",
+    "url": "/nguyen-tuan-art/static/media/IMG_9948.1521f8d1.jpg"
+  },
+  {
+    "revision": "8136d001a2edb34f75e34d241f217fa5",
+    "url": "/nguyen-tuan-art/static/media/cau_tinh_yeuPham_Mai_Linh_1.8136d001.jpg"
+  },
+  {
+    "revision": "8272c3dbaf26e11c68f6d5b7bfb66fcd",
+    "url": "/nguyen-tuan-art/static/media/hoi_an__Pham_Mai_Linh.8272c3db.jpg"
+  },
+  {
+    "revision": "ebb0d86fde356d95abaf64ba6855f44a",
+    "url": "/nguyen-tuan-art/static/media/deo_hai_van__dtdquynh__.ebb0d86f.jpg"
+  },
+  {
+    "revision": "2b7eb93b05d231c683fa4696aeeb1cfc",
+    "url": "/nguyen-tuan-art/static/media/ba_na_cau_vangchch_chloe.2b7eb93b.jpg"
+  },
+  {
+    "revision": "b2851db615b988a78a8de83339f51559",
+    "url": "/nguyen-tuan-art/static/media/cong_vien_bien_dong__haphuoong.b2851db6.jpg"
+  },
+  {
+    "revision": "79ad06913817e8502c6796bd79a59292",
+    "url": "/nguyen-tuan-art/static/media/son_trachua_linh_unghanbt2.79ad0691.jpg"
+  },
+  {
+    "revision": "d7fdafbe93c5fa20c2235bdd9b6a59c1",
+    "url": "/nguyen-tuan-art/static/media/thuan-phuoc-feild.d7fdafbe.jpg"
+  },
+  {
+    "revision": "686fe8e8557b49c180cdc622d3727299",
+    "url": "/nguyen-tuan-art/static/media/hai-van-quang.686fe8e8.jpg"
+  },
+  {
+    "revision": "5f6d1321638985e3289bfc310d4f25e6",
+    "url": "/nguyen-tuan-art/static/media/logo-tuan-art.5f6d1321.png"
+  },
+  {
+    "revision": "2a0e3b8b285e7e7c6a3212197881cc7a",
+    "url": "/nguyen-tuan-art/index.html"
+  }
+];
